0a6164f5416f7fb0e18e41afc081e195c35ccc61
Zackery Steck <zackery.steck@etegent.com>
2020-09-03 09:00:52 -0400

    Added back the crappy error handling to skip images rather than erroring out completely

--- stylize.py
+++ stylize.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
-
+import os
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -115,88 +115,98 @@ def main():
     # disable decompression bomb errors
     Image.MAX_IMAGE_PIXELS = None
     skipped_imgs = []
-    
+    num_written = 0
     # actual style transfer as in AdaIN
     with tqdm(total=len(content_paths)) as pbar:
         for content_path in content_paths:
-            # try:
-            content_img = Image.open(content_path).convert('RGB')
-            for style_path in random.sample(styles, args.num_styles):
-                content = content_tf(content_img)
+            try:
+                content_img = Image.open(content_path).convert('RGB')
+                for style_path in random.sample(styles, args.num_styles):
+                    content = content_tf(content_img)
 
-                style_img = Image.open(style_path).convert('RGB')
-                style = style_tf(style_img)
-                style = style.to(device).unsqueeze(0)
-                content = content.to(device).unsqueeze(0)
+                    style_img = Image.open(style_path).convert('RGB')
+                    style = style_tf(style_img)
+                    style = style.to(device).unsqueeze(0)
+                    content = content.to(device).unsqueeze(0)
 
-                # Loop until stylized image is above ssim_thresh or N times, whichever comes first
-                n_retries = 0
-                curr_ssim = 0
-                outputs = []
-                output_ssims = []
-                while curr_ssim < args.ssim_threshold and n_retries < args.n_retries:
-                    if n_retries > 0:
-                        style_path = random.sample(styles, 1)[0]
-                        style_img = Image.open(style_path).convert('RGB')
-                        style = style_tf(style_img)
-                        style = style.to(device).unsqueeze(0)
+                    # Loop until stylized image is above ssim_thresh or N times, whichever comes first
+                    n_retries = 0
+                    curr_ssim = 0
+                    outputs = []
+                    output_ssims = []
+                    while curr_ssim < args.ssim_threshold and n_retries < args.n_retries:
+                        if n_retries > 0:
+                            style_path = random.sample(styles, 1)[0]
+                            style_img = Image.open(style_path).convert('RGB')
+                            style = style_tf(style_img)
+                            style = style.to(device).unsqueeze(0)
 
-                    with torch.no_grad():
-                        output = style_transfer(vgg, decoder, content, style,
-                                                args.alpha)
-                    output = output.cpu()
-                    # Get the source image as numpy
-                    np_content = np.array(content_img)
-                    # Take the output image and resize so the dimensions match the content
-                    np_output = output[0, :]
-                    # Make sure the shape is (W, H, C)
-                    np_output = np_output.transpose(0, 2).transpose(0, 1).numpy()
-                    np_output = Image.fromarray(np.uint8(np_output * 255))
-                    np_output = np.array(np_output.resize(np_content.shape[:2][::-1]))
+                        with torch.no_grad():
+                            output = style_transfer(vgg, decoder, content, style,
+                                                    args.alpha)
+                        output = output.cpu()
+                        # Get the source image as numpy
+                        np_content = np.array(content_img)
+                        # Take the output image and resize so the dimensions match the content
+                        np_output = output[0, :]
+                        # Make sure the shape is (W, H, C)
+                        np_output = np_output.transpose(0, 2).transpose(0, 1).numpy()
+                        np_output = Image.fromarray(np.uint8(np_output * 255))
+                        np_output = np.array(np_output.resize(np_content.shape[:2][::-1]))
 
-                    # Compute the ssim between the content and the output
-                    curr_ssim = ssim(np_content, np_output, data_range=np_output.max() - np_output.min(),
-                                     multichannel=True)
-                    # Store the output and the current ssim
-                    outputs.append(output)
-                    output_ssims.append(curr_ssim)
-                    n_retries += 1
+                        # Compute the ssim between the content and the output
+                        curr_ssim = ssim(np_content, np_output, data_range=np_output.max() - np_output.min(),
+                                         multichannel=True)
+                        # Store the output and the current ssim
+                        outputs.append(output)
+                        output_ssims.append(curr_ssim)
+                        n_retries += 1
 
-                # If the last ssim val is less than the threshold, select the output image (this needs to be assigned to the output variable
-                if curr_ssim < args.ssim_threshold:
-                    output_ssims = np.array(output_ssims)
-                    best_idx = output_ssims.argmax()
-                    output = outputs[best_idx]
-                    print(
-                        f'=> No image passed threshold after {n_retries}. Taking best image with {output_ssims[best_idx]} SSIM value')
+                    if (len(output_ssims) < args.n_retries and len(output_ssims) > 5) and num_written < 5:
+                        worst = np.array(output_ssims).argmin()
+                        base_name = os.path.basename(str(content_path)).split('.')[0]
+                        save_image(outputs[worst], f'/media/zsteck/storage/lowshot/experiments/synthetic/{base_name}_rejected.png',
+                               padding=0)
+                        save_image(output, f'/media/zsteck/storage/lowshot/experiments/synthetic/{base_name}_accepted.png', padding=0)
+                        save_image(content.cpu(), f'/media/zsteck/storage/lowshot/experiments/synthetic/{base_name}.png',
+                                   padding=0)
+                        num_written += 1
 
-                rel_path = content_path.relative_to(content_dir)
-                out_dir = output_dir.joinpath(rel_path.parent)
+                    # If the last ssim val is less than the threshold, select the output image (this needs to be assigned to the output variable
+                    if curr_ssim < args.ssim_threshold:
+                        output_ssims = np.array(output_ssims)
+                        best_idx = output_ssims.argmax()
+                        output = outputs[best_idx]
+                        print(
+                            f'=> No image passed threshold after {n_retries}. Taking best image with {output_ssims[best_idx]} SSIM value')
 
-                # create directory structure if it does not exist
-                if not out_dir.is_dir():
-                    out_dir.mkdir(parents=True)
+                    rel_path = content_path.relative_to(content_dir)
+                    out_dir = output_dir.joinpath(rel_path.parent)
 
-                content_name = content_path.stem
-                style_name = style_path.stem
-                out_filename = content_name + '-stylized-' + style_name + content_path.suffix
-                output_name = out_dir.joinpath(out_filename)
+                    # create directory structure if it does not exist
+                    if not out_dir.is_dir():
+                        out_dir.mkdir(parents=True)
 
-                save_image(output, output_name, padding=0)  # default image padding is 2.
-                style_img.close()
-            content_img.close()
-            # except OSError as e:
-            #     print(e)
-            #     print('Skipping stylization of %s due to an error' %(content_path))
-            #     skipped_imgs.append(content_path)
-            #     continue
-            # except RuntimeError as e:
-            #     print(e)
-            #     print('Skipping stylization of %s due to an error' %(content_path))
-            #     skipped_imgs.append(content_path)
-            #     continue
-            # finally:
-            pbar.update(1)
+                    content_name = content_path.stem
+                    style_name = style_path.stem
+                    out_filename = content_name + '-stylized-' + style_name + content_path.suffix
+                    output_name = out_dir.joinpath(out_filename)
+
+                    save_image(output, output_name, padding=0)  # default image padding is 2.
+                    style_img.close()
+                content_img.close()
+            except OSError as e:
+                print(e)
+                print('Skipping stylization of %s due to an error' %(content_path))
+                skipped_imgs.append(content_path)
+                continue
+            except RuntimeError as e:
+                print(e)
+                print('Skipping stylization of %s due to an error' %(content_path))
+                skipped_imgs.append(content_path)
+                continue
+            finally:
+                pbar.update(1)
             
     if(len(skipped_imgs) > 0):
         with open(output_dir.joinpath('skipped_imgs.txt'), 'w') as f:
